b4dcd193cfc3fee3c38352ee1846f2850ca5c3c9
mustasaari <36700246+mustasaari@users.noreply.github.com>
2019-12-18 13:06:39 +0200

    Add GPS location
    gps

---
+++ BusRadar/BusMarker.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {StyleSheet, Image, Text} from 'react-native';
+import {Marker} from 'react-native-maps';
+
+export default class BusMarker extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            tracksViewChanges: true,
+        }
+        console.log(this.props.coordinates)
+    }
+
+    componentDidMount() {
+        console.log("Mount");
+    }
+
+    stopRendering = () =>
+    {
+        this.setState({ tracksViewChanges: false });
+    }
+
+    render()
+    {
+
+        return (
+            <Marker
+                key={this.props.key}
+                coordinate={this.props.coordinate}
+                title={this.props.title}
+                tracksViewChanges={false}
+            >
+            <Text style={{borderColor: '#ff0000', borderWidth: 1}}>{this.props.title}</Text>
+            </Marker>
+        )
+    }
+
+}
+
+
+const styles = StyleSheet.create({
+    mapMarker:
+    {
+        width: 36,
+        height: 50,
+    },
+});
--- BusRadar/MapComponent.js
+++ BusRadar/MapComponent.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import {Dimensions} from "react-native";
 import { Marker } from 'react-native-maps';
+import BusMarker from './BusMarker';
 
 export default class MapComponent extends React.Component {
 
@@ -23,7 +24,23 @@ export default class MapComponent extends React.Component {
     componentDidMount() {
         //fetch(url).then(response => response.json()).then(data => this.setState( {fetcheddata: data} ));
         this.getBusses();
+        this.findCoordinates();
     }
+
+    findCoordinates = () => {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+                const location = JSON.stringify(position);
+                //const location = position;
+                console.log("gps function");
+                console.log(typeof location);
+                console.log(typeof position);
+                this.setState({userlocation: position });
+                console.log("---------------------" +location);
+			},
+			error => console.log("error gps")
+		);
+	};
 
     async getBusses() {
         await fetch('http://data.itsfactory.fi/journeys/api/1/vehicle-activity').then(response => response.json()).then(data => this.setState({busdata: data}));
@@ -31,12 +48,13 @@ export default class MapComponent extends React.Component {
         let newMarkers = [];
         console.log("fetch complete " +this.state.busdata.body.length);
         for (var i = 0; i < this.state.busdata.body.length; i++) {
-            console.log(this.state.busdata.body[i].monitoredVehicleJourney.journeyPatternRef +"   " +this.state.busdata.body[i].monitoredVehicleJourney.vehicleLocation.latitude +"   " +this.state.busdata.body[i].monitoredVehicleJourney.vehicleLocation.longitude);
+            //console.log(this.state.busdata.body[i].monitoredVehicleJourney.journeyPatternRef +"   " +this.state.busdata.body[i].monitoredVehicleJourney.vehicleLocation.latitude +"   " +this.state.busdata.body[i].monitoredVehicleJourney.vehicleLocation.longitude);
             newMarkers.push( {latlng:{latitude: parseFloat(this.state.busdata.body[i].monitoredVehicleJourney.vehicleLocation.latitude), longitude: parseFloat(this.state.busdata.body[i].monitoredVehicleJourney.vehicleLocation.longitude)}, title: this.state.busdata.body[i].monitoredVehicleJourney.journeyPatternRef, subtitle: 'jee', description: 'kuvaus', key: "i"+i} )
         }
-        console.log("for loop finished " +fetchdata.body.length);
+        //console.log("for loop finished " +fetchdata.body.length +" coords : " +this.state.userlocation.coords.longitude + " " +this.state.userlocation.coords.latitude);
         this.setState( {markers : newMarkers} );
 
+        this.findCoordinates();
         setTimeout(this.getBusses,20000);
     }
 
@@ -56,7 +74,11 @@ export default class MapComponent extends React.Component {
               }} style={{height: 800, width: 400}}>
               {/*}}} style={{flex: 1,height: 500, width: Math.floor(Dimensions.get('window').width)}}>*/}
       
-              {this.state.markers.map(marker => (<Marker coordinate={marker.latlng} title={marker.title} description="" key={marker.key}/>))}
+              {this.state.markers.map(marker => (<Marker coordinate={marker.latlng} title={marker.title} description="" key={marker.key} tracksViewChanges={false}>
+                  <Text style={styles.bus}>{marker.title}</Text>
+                  </Marker>))}
+              {/*this.state.markers.map(marker => (<BusMarker coordinate={marker.latlng} title={marker.title} description="kek" key={marker.key}/>))*/}
+              {typeof this.state.userlocation !== "undefined" ? <Marker coordinate={{latitude: this.state.userlocation.coords.latitude, longitude: this.state.userlocation.coords.longitude}}/> : console.log("no user location available")}
       
               </MapView>
           </View>
@@ -72,5 +94,16 @@ const styles = StyleSheet.create({
       backgroundColor: 'lightgreen',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    bus: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: '#1d5e8c',
+        borderRadius: 20,
+        padding: 8,
+        backgroundColor: "#a1c6e0",
+        width: 36,
+        height: 36,
+        textAlign: 'center',
     },
   });
